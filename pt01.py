919c83c4655e60793d9c39f0edad908d2ade4056
ArthurMouraSales <arthur.mourasales@gmail.com>
2024-09-12 14:53:40 -0300

    adicao do while

--- pt01.py
+++ pt01.py
@@ -1,22 +1,22 @@
-num1 = float(input("Digite um valor: "))
-num2 = float(input("Digite outro valor: "))
-resp = input("Qual operação você quer realizar? ")
-
-if resp == "adicao":
-    adicao = num1 + num2
-    print(f"{adicao}")
-elif resp == "subtracao":
-    sub = num1 - num2
-    print(f"{sub}")
-elif resp == "multiplicacao":
-    mult = num1 * num2
-    print(f"{mult}")
-elif resp == "divisao":
-    if num2 == 0:
-        print("Não é possivel dividir por 0")
+while True:
+    num1 = float(input("Digite um valor: "))
+    num2 = float(input("Digite outro valor"))
+    resp = input("Qual operação você quer realizar? ")
+    if resp == "adicao":
+        adicao = num1 + num2
+        print(f"{adicao}")
+    elif resp == "subtracao":
+        sub = num1 - num2
+        print(f"{sub}")
+    elif resp == "multiplicacao":
+        mult = num1 * num2
+        print(f"{mult}")
+    elif resp == "divisao":
+        if num2 == 0:
+            print("Não é possivel dividir por 0")
+        else:
+            div = num1 / num2
+            print(f"{div}")
     else:
-        div = num1 / num2
-        print(f"{div}")
-else:
-    print("Erro na digitação da operação")
+        print("Erro na digitação da operação")
 
