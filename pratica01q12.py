5571b90d83a6e7a2ab02b81cd00b38aa37dbf49e
ArthurMouraSales <arthur.mourasales@gmail.com>
2024-10-04 20:48:37 -0300

    teste

--- pratica01q12.py
+++ pratica01q12.py
@@ -1,3 +1,5 @@
 def converter():
     escala = input("Digite a escala desejada|C - Celsius|F - Fahrenheit|: ").upper()
-    
+    temperatura = int(input("Digite a temperatura a ser convertida: "))
+    if escala == "C":
+        
