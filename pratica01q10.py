1f3879cde7b98fd01eef51be176015e8340534fc
ArthurMouraSales <arthur.mourasales@gmail.com>
2024-10-04 22:47:28 -0300

    questao 10

--- pratica01q10.py
+++ pratica01q10.py
@@ -1,1 +1,8 @@
-#Questão 1
+#Questão 10
+def contador():
+    palavras = texto.strip().split()
+    return len(palavras)
+
+texto = input("Digite seu texto: ")
+
+print(f"Número de palavras: {contador()}")
